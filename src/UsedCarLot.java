1bd073ca39bc2071b0e9ae6e739daa0921d5850e
Rohit <rohitc@nycstudents.net>
2023-02-01 11:43:25 -0500

    the real final commit

--- src/UsedCarLot.java
+++ src/UsedCarLot.java
@@ -27,4 +27,28 @@ public class UsedCarLot {
         }
 
     }
+
+    public void addCar(int indexToAdd, Car carToAdd)
+    {
+        inventory.add(indexToAdd, carToAdd);
+    }
+    public Car sellCarShift(int indexOfCarToSell)
+    {
+        Car sold = inventory.remove(indexOfCarToSell);
+        return sold;
+    }
+
+    public Car sellCarNoShift(int indexOfCarToSell)
+    {
+        Car sold = inventory.get(indexOfCarToSell);
+        inventory.set(indexOfCarToSell, null);
+        return sold;
+    }
+
+    public void moveCar(int indexOfCarToMove, int destinationIndex)
+    {
+        Car move = inventory.remove(indexOfCarToMove);
+        inventory.add(destinationIndex, move);
+    }
+
 }
